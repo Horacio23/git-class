64e9a2bf298656ee191c37a1c87455ebaea1bd12
Horacio Delgado <horacio.delgado23@gmail.com>
2016-07-06 16:08:51 -0400

    added the git lg alias, was missing from previous commits

--- GitSettingsUnix.sh
+++ GitSettingsUnix.sh
@@ -6,6 +6,7 @@ git config --global alias.co "checkout"
 git config --global alias.dp "diff --word-diff --unified=10"
 git config --global alias.so "show --pretty='parent %Cred%p%Creset commit %Cred%h%Creset%C(yellow)%d%Creset%n%n%w(72,2,2)%s%n%n%w(72,0,0)%C(cyan)%an%Creset %Cgreen%ar%Creset'"
 git config --global alias.unstage "reset Head"
+git config --global alias.lg "log --pretty='%C(red)%h%Creset | %C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %C(cyan) [%an]%Creset' --graph"
 
 #Configurations
 git config --global core.pager 'less -RFX'

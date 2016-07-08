ccf4572bda386dfe2d24d5b7f814f56cfc703023
Horacio Delgado <horacio.delgado23@gmail.com>
2016-07-08 10:45:41 -0400

    fixed the error with the windows script. I was using single quotes for the pager config, it needed double

--- GitSettingsWindows.bat
+++ GitSettingsWindows.bat
@@ -4,11 +4,11 @@ git config --global alias.cma "commit --all -m"
 git config --global alias.qm '!git checkout $1;git merge @{-1}'
 git config --global alias.co "checkout"
 git config --global alias.dp "diff --word-diff --unified=10"
-git config --global alias.so "show --pretty='parent %Cred%p%Creset commit %Cred%h%Creset%C(yellow)%d%Creset%n%n%w(72,2,2)%s%n%n%w(72,0,0)%C(cyan)%an%Creset %Cgreen%ar%Creset'"
+git config --global alias.so "show --pretty='parent %%Cred%%p%%Creset commit %%Cred%%h%%Creset%%C(yellow)%%d%%Creset%%n%%n%%w(72,2,2)%%s%%n%%n%%w(72,0,0)%%C(cyan)%%an%%Creset %%Cgreen%%ar%%Creset'"
 git config --global alias.unstage "reset Head"
-git config --global alias.lg "log --pretty='%C(red)%h%Creset | %C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %C(cyan) [%an]%Creset' --graph"
+git config --global alias.lg "log --pretty='%%C(red)%%h%%Creset | %%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr)%%Creset %%C(cyan) [%%an]%%Creset' --graph"
 
 ::Configurations
-git config --global core.pager 'less -RFX'
+git config --global core.pager "less -RFX"
 git config --global diff.algorithm histogram
 git config --global core.whitepsace "blank-at-eol,blank-at-eof, indent-with-non-tab"
